c4465c09b8ad43e3d7aab967cafffee9c886eb59
Jp <jpsiegel8@gmail.com>
2022-10-16 16:18:33 -0400

    v1.0.8

--- scripts/afterSignHook.js
+++ scripts/afterSignHook.js
@@ -27,6 +27,7 @@ module.exports = async function (params) {
     await electron_notarize.notarize({
       appBundleId: appId,
       appPath: appPath,
+      appleApiKey: process.env.API_KEY,
       appleApiKeyId: process.env.API_KEY_ID,
       appleApiIssuer: process.env.API_KEY_ISSUER_ID,
     });
